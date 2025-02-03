08d59453d791a5c792d39fbdc8a8a9a44417e2f2
Simplilearn_Github <siddam.bharat@simplilearn.net>
2025-02-03 19:06:49 +0530

    commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem incidunt voluptate vero, sed laudantium cupiditate repudiandae alias amet nisi.</p>
+            <p> At TastyGo, we bring you delicious meals from your favorite restaurants, 
+              delivered fresh and fast to your doorstep. Quality ingredients, amazing flavors, 
+              and hassle-free ordering</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
